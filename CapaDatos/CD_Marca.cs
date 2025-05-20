2aa1ddf6a0e4401897cea5eca47aa3df630de102
Vaqui26 <nicolasvaccaro48@yahoo.com.ar>
2025-05-19 22:18:22 -0300

    Finalizacion de la pagina Categoria, creacion en la capa de Datos y Negocio para la Marca

---
+++ CapaDatos/CD_Marca.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+
+namespace CapaDatos
+{
+    public class CD_Marca
+    {
+        private List<Marca> listaMarcas;
+
+        public List<Marca> listar()
+        {
+            listaMarcas = new List<Marca>();
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = "Select IdMarca, Descripcion, Activo From MARCA";
+                    SqlCommand cmd = new SqlCommand(query, oConexion);
+                    cmd.CommandType = CommandType.Text;
+                    oConexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            listaMarcas.Add(
+                               new Marca
+                               {
+                                   IdMarca = int.Parse(dr["IdMarca"].ToString()),
+                                   Descripcion = dr["Descripcion"].ToString(),
+                                   Activo = bool.Parse(dr["Activo"].ToString())
+                               }
+                            );
+                        }
+                    }
+                }
+                return listaMarcas;
+            }
+            catch (Exception)
+            {
+                return listaMarcas;
+            }
+        }
+        public int Registrar(Marca marca, out string Mensaje)
+        {
+
+            int idAutogenerado = 0;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarMarca", oConexion);
+                    cmd.Parameters.AddWithValue("Descripcion", marca.Descripcion);
+                    cmd.Parameters.AddWithValue("Activo", marca.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    idAutogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                idAutogenerado = 0;
+                Mensaje = ex.Message;
+            }
+            return idAutogenerado;
+
+        }
+        public bool Editar(Marca marca, out string Mensaje)
+        {
+
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EditarMarca", oConexion);
+                    cmd.Parameters.AddWithValue("IdMarca", marca.IdMarca);
+                    cmd.Parameters.AddWithValue("Descripcion", marca.Descripcion);
+                    cmd.Parameters.AddWithValue("Activo", marca.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Mensaje = ex.Message;
+            }
+            return resultado;
+
+        }
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EliminarMarca", oConexion);
+                    cmd.Parameters.AddWithValue("IdMarca", id);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ CapaNegocio/CN_Marca.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class CN_Marca
+    {
+        private CD_Marca objCapaDatos = new CD_Marca();
+
+        public List<Marca> Listar()
+        {
+            return objCapaDatos.listar();
+        }
+        public int Registrar(Marca marca, out string Mensaje)
+
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(marca.Descripcion) || string.IsNullOrWhiteSpace(marca.Descripcion))
+            {
+                Mensaje = "La descripcion de la marca no puede ser vacia!";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDatos.Registrar(marca, out Mensaje);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public bool Editar(Marca marca, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(marca.Descripcion) || string.IsNullOrWhiteSpace(marca.Descripcion))
+            {
+                Mensaje = "La descripcion de la marca no puede ser vacia!";
+            }
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDatos.Editar(marca, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            return objCapaDatos.Eliminar(id, out Mensaje);
+        }
+    }
+}
